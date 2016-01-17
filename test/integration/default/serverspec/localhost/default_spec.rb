e389d36a15c6aafaa6d7afa2e2c125f2cb166023
Ben Visser <theodore.r.visser@gmail.com>
2016-01-17 01:12:22 -0500

    kitchen testing

---
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe 'ansible-nginx::default' do
+
+  # describe package('ruby2.2') do
+  #   it { should be_installed.by('apt') }
+  # end
+
+  # describe package('ruby2.2-dev') do
+  #   it { should be_installed.by('apt') }
+  # end
+
+  # describe package('bundler') do
+  #   it { should be_installed.by('gem') }
+  # end
+
+  # describe ppa('brightbox/ruby-ng') do
+  #   it { should exist }
+  # end
+end
